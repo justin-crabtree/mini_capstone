1c31443f7d220d8b1f40a2fd412b96ee6d5811d5
crabtree <justin.kyle.crabtree@gmail.com>
2019-09-27 13:17:45 -0400

    rearranges seed file and adds product id to image file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,17 @@ Category.create!([
   {name: "Action"},
   {name: "FPS"}
 ])
-ProductCategory.create!([
-  {product_id: 1, category_id: 1},
-  {product_id: 1, category_id: 3},
-  {product_id: 2, category_id: 3}
-])
 User.create!([
   {name: "Alexis", email: "acrabtree@gmail.com", password_digest: "$2a$12$eeJ3vbaylW8fPlRoWLDDF.VumRqRHRNSLpelMGbUHkAf0A17Zq.ue", admin: false},
   {name: "Justin", email: "jcrabtree@gmail.com", password_digest: "$2a$12$jpAb3BDlGuavmIpSbQjcu.pMQqppa55SQbQz1.YKiFYm2/aw9FCp6", admin: true}
 ])
-Supplier.create!([
-  {name: "Amazon", email: "amazon@gmail.com", phone_number: "223-445-6677"},
-  {name: "Gamestop", email: "gamestop@gmail.com", phone_number: "123-908-4789"}
-])
-Image.create!([
-  {url: "https://static.gamespot.com/uploads/original/1591/15918215/3545029-cyberpunk_02.jpg", product_id: nil}
-])
 Order.create!([
   {user_id: 1, subtotal: "120.0", tax: "10.8", total: "130.8"},
   {user_id: 1, subtotal: "160.0", tax: "14.4", total: "174.4"}
+])
+Supplier.create!([
+  {name: "Amazon", email: "amazon@gmail.com", phone_number: "223-445-6677"},
+  {name: "Gamestop", email: "gamestop@gmail.com", phone_number: "123-908-4789"}
 ])
 Product.create!([
   {name: "Cyberpunk 2077", price: "59.0", description: "Adapted from the 1988 tabletop game Cyberpunk 2020, it is set fifty-seven years later in dystopian Night City, an open world with six distinct regions. In a first-person perspective, players assume the role of the customisable mercenary V, who can reach prominence in hacking, machinery, and combat. V has an arsenal of ranged weapons and options for melee combat.", in_stock: true, supplier_id: 1},
@@ -32,4 +24,11 @@ Product.create!([
   {name: "The Legend of Zelda: Link's Awakening", price: "60.0", description: "It is a remake of the 1993 game for the Game Boy that retains the original title's top-down perspective and gameplay, along with elements from the 1998 remaster Link's Awakening DX. The remake features a \"retro-modern\" art style unique within the series with toy-like character designs, diorama-like world designs, and tilt-shift visuals that evoke the original game's presentation on the Game Boy. It also features customizable dungeons which the player can create and then complete for rewards.", in_stock: true, supplier_id: 2},
   {name: "Final Fantasy 7 Remake", price: "60.0", description: "A remake of the classic RPG series for modern platforms.", in_stock: true, supplier_id: 1}
 ])
-
+Image.create!([
+  {url: "https://static.gamespot.com/uploads/original/1591/15918215/3545029-cyberpunk_02.jpg", product_id: 1}
+])
+ProductCategory.create!([
+  {product_id: 1, category_id: 1},
+  {product_id: 1, category_id: 3},
+  {product_id: 2, category_id: 3}
+])
